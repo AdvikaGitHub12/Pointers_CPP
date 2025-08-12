e50526d83ac24a5034ee6a9f98309c33479ea4f0
Advika Bhosle <advikabhosle@gmail.com>
2025-08-12 20:04:40 +0530

    Reversing Pointer

---
+++ Pointers_Reverse.cpp
@@ -0,0 +1,37 @@
+// Name: Advika Bhosle
+// PRN: 24070123007
+//Batch: EnTc A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100]; 
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int* ptr = arr + n - 1; 
+    cout << "Array in reverse using pointer:\n";
+    for (int i = 0; i < n; i++) {
+        cout << *ptr << " ";
+        ptr--;
+    }
+
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter number of elements: 5
+Enter 5 elements:
+25 36 49 64 81
+Array in reverse using pointer:
+81 64 49 36 25 
+
+=== Code Execution Successful ===
